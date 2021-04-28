a635f5b74889053ccebb0276f5d91a3046721e60
aleucilima <aleuci.lima@gmail.com>
2021-04-28 17:12:29 -0300

    Update Gym-Manager

--- routes.js
+++ routes.js
@@ -2,7 +2,15 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (request, response) => {
-    return response.send('ok')
+    return response.redirect('/instructors')
+})
+
+routes.get('/instructors', (request, response) => {
+    return response.render('instructors/index')
+})
+
+routes.get('/members', (request, response) => {
+    return response.send('members')
 })
 
 module.exports = routes
