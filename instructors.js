2cb652e09cc839756626634d15296ff6b84b709b
aleucilima <aleuci.lima@gmail.com>
2021-04-30 17:20:19 -0300

    Create database

---
+++ instructors.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const data = require('./data.json')
+
+exports.post = (request, response) => {
+        const keys = Object.keys(request.body)
+    
+        for(key of keys) {
+            if (request.body[key] == '' )
+                return response.send('Please, fill all fields')
+        }
+
+        data.instructors.push(request.body)
+
+        fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+            if (err) return response.send('Write file error!')
+
+            return response.redirect('/instructors')
+        })
+
+}
