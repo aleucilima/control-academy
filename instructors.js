1e18c6beb31621a1feab92ea58bf7c46a45c8249
aleucilima <aleuci.lima@gmail.com>
2021-04-30 18:04:48 -0300

    Update data.json

--- instructors.js
+++ instructors.js
@@ -5,11 +5,26 @@ exports.post = (request, response) => {
         const keys = Object.keys(request.body)
     
         for(key of keys) {
-            if (request.body[key] == '' )
+            if (request.body[key] == '')
                 return response.send('Please, fill all fields')
         }
+        
+        let { avatar_url, birth, name, services, gender } = request.body
 
-        data.instructors.push(request.body)
+        birth = Date.parse(birth)
+        const created_at = Date.now()
+        const id = Number(data.instructors.length + 1)
+
+
+        data.instructors.push({
+            id,
+            avatar_url,
+            name,
+            birth,
+            gender,
+            services,
+            created_at
+        })
 
         fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
             if (err) return response.send('Write file error!')
