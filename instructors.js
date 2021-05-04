beeead88ba13eb82236224eb9d8fa3688992caa9
aleucilima <aleuci.lima@gmail.com>
2021-05-04 17:50:20 -0300

    Working with date

--- instructors.js
+++ instructors.js
@@ -8,9 +8,24 @@ exports.show = (request, response) => {
 
     if(!foundInstructor) return response.send('Instructor not found')
 
+    //Estrutura para trabalhar com datas de aniversário
+    function age(timestamp) {
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        const month = today.getMonth() - birthDate.getMonth()
+
+        if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()){
+            age = age - 1
+        }
+
+        return age
+    }
+
     const instructor = {
         ...foundInstructor,
-        age: '',
+        age: age(foundInstructor.birth),
         services: foundInstructor.services.split(','),
         created_at: '',
     }
