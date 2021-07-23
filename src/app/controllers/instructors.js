3f5a57836cd40018b213138af77a22366881d8a5
aleucilima <aleuci.lima@gmail.com>
2021-07-23 13:57:42 -0300

    Refatorando Gym-manager

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,16 +1,13 @@
-const fs = require('fs')
-const data = require('../data.json')
-const { age, date } = require('../utils')
+const { age, date } = require('../../lib/utils')
 
-exports.index = (request, response) => {
-    return response.render('instructors/index', { instructors: data.instructors })
-}
-
-exports.create =  (request, response) => {
-    return response.render('instructors/create')
-}
-
-exports.post = (request, response) => {
+module.exports = {
+    index(request, response) {
+        return response.render('instructors/index', { instructors })
+    },
+    create(request, response) {
+        return response.render('instructors/create')
+    },
+    post(request, response) {
         const keys = Object.keys(request.body)
     
         for(key of keys) {
@@ -20,102 +17,26 @@ exports.post = (request, response) => {
         
         let { avatar_url, birth, name, services, gender } = request.body
 
-        birth = Date.parse(birth)
-        const created_at = Date.now()
-        const id = Number(data.instructors.length + 1)
+        return
 
-
-        data.instructors.push({
-            id,
-            avatar_url,
-            name,
-            birth,
-            gender,
-            services,
-            created_at
-        })
-
-        fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-            if (err) return response.send('Write file error!')
-
-            return response.redirect('/instructors')
-        })
-
-}
-
-exports.show = (request, response) => {
-    const { id } = request.params
-
-    const foundInstructor = data.instructors.find((instructor) => instructor.id == id)
-
-    if(!foundInstructor) return response.send('Instructor not found')
-
-    const instructor = {
-        ...foundInstructor,
-        age: age(foundInstructor.birth),
-        services: foundInstructor.services.split(','),
-        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
-    }
-
-    return response.render('instructors/show', { instructor })
-}
-
-exports.edit = (request, response) => {
-    const { id } = request.params
-
-    const foundInstructor = data.instructors.find((instructor) => instructor.id == id)
-
-    if(!foundInstructor) return response.send('Instructor not found')
+    },
+    show(request, response) {
+        return
+    },
+    edit(request, response) {
+        return
+    },
+    put(request, response) {
+        const keys = Object.keys(request.body)
     
-    const instructor = {
-        ...foundInstructor,
-        birth: date(foundInstructor.birth).iso
-    }
-    
-    return response.render('instructors/edit', { instructor })
-}
-
-exports.put = (request, response) => {
-    const { id } = request.body
-    let index = 0
-
-    const foundInstructor = data.instructors.find((instructor, foundIndex) => {
-        if (instructor.id == id) {
-            index = foundIndex
-            return true
+        for(key of keys) {
+            if (request.body[key] == '')
+                return response.send('Please, fill all fields')
         }
-    })
 
-    if(!foundInstructor) return response.send('Instructor not found')
-
-    const instructor = {
-        ...foundInstructor,
-        ...request.body,
-        birth: Date.parse(request.body.birth),
-        id: Number(request.body.id)
+        return
+    },
+    delete(request, response) {
+        return
     }
-
-    data.instructors[index] = instructor
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-        if (err) return response.send('Write error!')
-        
-        return response.redirect(`/instructors/${id}`)
-    })
-
-}
-
-exports.delete = (request, response) => {
-    const { id } = request.body
-
-    const filteredInstructors = data.instructors.filter((instructor) => instructor.id != id)
-
-
-    data.instructors = filteredInstructors
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-        if (err) return response.send('Write file error')
-
-        return response.redirect('/instructors')
-    })
 }
--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,16 +1,13 @@
-const fs = require('fs')
-const data = require('../data.json')
-const { date } = require('../utils')
+const { age, date } = require('../../lib/utils')
 
-exports.index = (request, response) => {
-    return response.render('members/index', { members: data.members })
-}
-
-exports.create = (request, response) => {
-    return response.render('members/create')
-}
-
-exports.post = (request, response) => {
+module.exports = {
+    index(request, response) {
+        return response.render('members/index', { members })
+    },
+    create(request, response) {
+        return response.render('members/create')
+    },
+    post(request, response) {
         const keys = Object.keys(request.body)
     
         for(key of keys) {
@@ -18,101 +15,26 @@ exports.post = (request, response) => {
                 return response.send('Please, fill all fields')
         }
 
-        birth = Date.parse(request.body.birth)
+        return
 
-        let id = 1
-        const lastMember = data.members[data.members.length - 1]
-
-        if(lastMember) {
-            id = lastMember.id + 1
+    },
+    show(request, response) {
+        return
+    },
+    edit(request, response) {
+        return
+    },
+    put(request, response) {
+        const keys = Object.keys(request.body)
+    
+        for(key of keys) {
+            if (request.body[key] == '')
+                return response.send('Please, fill all fields')
         }
 
-
-        data.members.push({
-            id,
-            ...request.body,
-            birth
-        })
-
-        fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-            if (err) return response.send('Write file error!')
-
-            return response.redirect('/members')
-        })
-
-}
-
-exports.show = (request, response) => {
-    const { id } = request.params
-
-    const foundMember = data.members.find((member) => member.id == id)
-
-    if(!foundMember) return response.send('Member not found')
-
-    const member = {
-        ...foundMember,
-        birth: date(foundMember.birth).birthDay,
+        return
+    },
+    delete(request, response) {
+        return
     }
-
-    return response.render('members/show', { member })
-}
-
-exports.edit = (request, response) => {
-    const { id } = request.params
-
-    const foundMember = data.members.find((member) => member.id == id)
-
-    if(!foundMember) return response.send('Member not found')
-    
-    const member = {
-        ...foundMember,
-        birth: date(foundMember.birth).iso
-    }
-    
-    return response.render('members/edit', { member })
-}
-
-exports.put = (request, response) => {
-    const { id } = request.body
-    let index = 0
-
-    const foundMember = data.members.find((member, foundIndex) => {
-        if (member.id == id) {
-            index = foundIndex
-            return true
-        }
-    })
-
-    if(!foundMember) return response.send('Member not found')
-
-    const member = {
-        ...foundMember,
-        ...request.body,
-        birth: Date.parse(request.body.birth),
-        id: Number(request.body.id)
-    }
-
-    data.members[index] = member
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-        if (err) return response.send('Write error!')
-        
-        return response.redirect(`/members/${id}`)
-    })
-
-}
-
-exports.delete = (request, response) => {
-    const { id } = request.body
-
-    const filteredMembers = data.members.filter((member) => member.id != id)
-
-
-    data.members = filteredMembers
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-        if (err) return response.send('Write file error')
-
-        return response.redirect('/members')
-    })
 }
